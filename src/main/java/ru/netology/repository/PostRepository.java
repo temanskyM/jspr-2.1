c198ad7baf6914971256162bb54222fb915a824e
Artiom <temansky@yandex.ru>
2021-06-14 17:11:34 +0300

    Add spring di-annnotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
-// Stub
+@Repository
 public class PostRepository {
     private final ConcurrentHashMap<Long, Post> map = new ConcurrentHashMap<>();
     private long id;
